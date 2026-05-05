d7cfb9997f711c2ecd02a710695ebabed36abc2d
Mohammedzv <mf9391224@gmail.com>
2026-05-05 13:43:58 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const path = require('path');
-const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => { socket.broadcast.emit('chat message', msg); });
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
 });
-http.listen(PORT, () => { console.log(`Server running at http://localhost:${PORT}`); });
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+    socket.on('chat message', (msg) => {
+        socket.broadcast.emit('chat message', msg);
+    });
+});
+
+http.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
